be45c954a6e7a9f220ef5c36663ca1ead6fad269
Yerkin Zhaksylykov <yerkinzhaksylykov@gmail.com>
2021-07-08 19:47:25 +0600

    shopping

---
+++ functions/hello.js
@@ -0,0 +1,19 @@
+
+// domain/.netlify/functions/hello
+const items = [
+{
+  id:1,
+  name:"jon"
+},
+{
+  id:2,
+  name:"susan"
+}
+]
+
+exports.handler = async function(event,context){
+  return {
+    statusCode:200,
+    body:"hello world",
+  }
+}
---
+++ src/context/user_context.js
@@ -0,0 +1,34 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+
+const UserContext = React.createContext()
+export const UserProvider = ({ children }) => {
+  
+  const {
+        //isAuthenticated,
+        loginWithRedirect,
+        logout, 
+        user,
+        //isLoading
+      } = useAuth0();
+
+  const [myUser,setMyUser] = useState(null);
+
+  useEffect(()=>{
+    setMyUser(user);  
+    // if(isAuthenticated){
+    //   setMyUser(user)
+    // }
+    // else{
+    //   setMyUser(false)
+    // }
+  },[user]);
+
+  return (
+    <UserContext.Provider value={{loginWithRedirect,logout,myUser}}>{children}</UserContext.Provider>
+  )
+}
+// make sure use
+export const useUserContext = () => {
+  return useContext(UserContext)
+}
